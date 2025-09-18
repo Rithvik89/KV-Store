319bc79006837739ea784b1bbf011fa5a2762999
Rithvik Reddy <rithvkalkanti@gmail.com>
2025-09-18 18:39:55 +0530

    Add logger implementation with multiple log levels for KVStore

---
+++ cmd/server/logger.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type LogLevel int
+
+const (
+	DEBUG LogLevel = iota
+	INFO
+	WARN
+	ERROR
+	FATAL
+)
+
+var logLevelNames = map[LogLevel]string{
+	DEBUG: "DEBUG",
+	INFO:  "INFO",
+	WARN:  "WARN",
+	ERROR: "ERROR",
+	FATAL: "FATAL",
+}
+
+type Logger struct {
+	component string
+	LogLevel  LogLevel
+}
+
+func NewLogger(component string) *Logger {
+	return &Logger{component: component, LogLevel: INFO}
+}
+
+// log formats and prints a message if its level is >= the current log level
+func (l *Logger) log(level LogLevel, format string, args ...interface{}) {
+	if level >= l.LogLevel {
+		now := time.Now().Format("2006-01-02 15:04:05.000")
+		message := fmt.Sprintf(format, args...)
+		fmt.Printf("[%s] %s [%s] %s\n", now, logLevelNames[level], l.component, message)
+	}
+}
+
+// Debug logs a debug-level message
+func (l *Logger) Debug(format string, args ...interface{}) {
+	l.log(DEBUG, format, args...)
+}
+
+// Info logs an info-level message
+func (l *Logger) Info(format string, args ...interface{}) {
+	l.log(INFO, format, args...)
+}
+
+// Warn logs a warning-level message
+func (l *Logger) Warn(format string, args ...interface{}) {
+	l.log(WARN, format, args...)
+}
+
+// Error logs an error-level message
+func (l *Logger) Error(format string, args ...interface{}) {
+	l.log(ERROR, format, args...)
+}
+
+// Fatal logs a fatal-level message
+func (l *Logger) Fatal(format string, args ...interface{}) {
+	l.log(FATAL, format, args...)
+}
